6d72fb371892724068229e2559d4a8a9fe34db73
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-05 22:07:16 +0100

    add bike.rb & bike_spec.rb, tests for: respontds to working; release a bike and then expects the bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  # it "respond to release bike" do
-  #   docking_station = DockingStation.new
-  #   expect(docking_station).to respond_to(:release_bike)
-  # end
+  it {is_expected.to respond_to(:release_bike) }
 
-  it { binding.pry; is_expected.to respond_to(:release_bike) }
+  it "release a bike and then expects the bike to be working" do
+    new_bike = subject.release_bike
+    expect(new_bike).to be_working
+  end
+
 end
-
-# things to research!
-# described_class
-# subject
-# why does it short syntax work without declaring a subject?
-# research implict and explicit subject
