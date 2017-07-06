d253b42fbc5bf162a77c9a95c72edd52492bf56a
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-06 13:08:11 +0100

    add a test to raise an error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,14 +3,16 @@ require_relative 'bike'
 class DockingStation
 
   attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail "No bikes available" unless @bike
+    @bike
   end
 
-  def dock(bike)
+  def dock(bike) # setter method
   #Use an instance variable to store the bike
   # in the 'state' of this instance
     @bike = bike
   end
-  
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,19 +4,29 @@ describe DockingStation do
   it {is_expected.to respond_to(:release_bike) }
 
   it "release a bike and then expects the bike to be working" do
-    bike = subject.release_bike
+    # 1. have a bike in the docking stationn
+    # 2. releasea bike
+    # 3. make an assertion on that bike
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument}
 
-  it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    #we want to return the bike to the dock
-    expect(subject.dock(bike)).to eq bike
+  describe '#release_bike' do
+    it "returns bikes" do
+      bike = Bike.new
+      subject.dock(bike)
+      #we want to return the bike to the dock
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it "should raise an error if no bikes available" do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
   end
 
   it {is_expected.to respond_to(:bike)}
-
 end
